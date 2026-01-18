ef5bab6fdb5825065d5d493574b5a1333a8041f3
aashish-blitz <aashish.dhiman@blitznow.in>
2026-01-18 18:11:01 +0530

    fix | main activity file

--- android/app/src/main/kotlin/com/example/expense_tracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ezapps.expense_tracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/ezapps/expanzify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ezapps.expanzify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
